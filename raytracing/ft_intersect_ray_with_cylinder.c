a3716092a64363799242c97b1ecec4903e55be2f
amaroni <amaroni@student.42.fr>
2021-07-14 22:33:14 +0200

    Toujours en cours de mise en place de cylindre

--- raytracing/ft_intersect_ray_with_cylinder.c
+++ raytracing/ft_intersect_ray_with_cylinder.c
@@ -11,15 +11,18 @@ t_float2 *ft_intersect_ray_with_cylinder(t_float3 origin, t_float3 direction, t_
 	float height;
 	float t;
 	float t_prime;
-	float ti1;
-	float ti2;
+	
+	t_float3 A; //Intersection parameter between cylinder normal and ray
+	t_float3 Pi1; // Premiere Intersection avec la surface du cylindre
+	t_float3 Pi2; // Seconde Intersection avec la surface du cylindre
 
-	ti1 = 0;
-	ti2 = 0;
+	//Initialiser A a INFINITY
+	A = (float)INFINITY;
 	normal = ft_get_shape_normal(cylinder);
 	center = ft_get_shape_center(cylinder);
 	radius = ft_get_shape_radius(cylinder);
 	height = cylinder->content->parser_cylinder.cy_h;
+
 	//Intersect with caps
 	tmp1 = ft_intersect_ray_with_disk(origin, direction, center, normal, radius);
 	tmp2 = ft_intersect_ray_with_disk(origin, direction, ft_float3_plus_float3(center, ft_float_times_float3(height, normal)), normal, radius);
@@ -29,6 +32,7 @@ t_float2 *ft_intersect_ray_with_cylinder(t_float3 origin, t_float3 direction, t_
 	free(tmp2);
 	if (solutions->t1 < (float)INFINITY || solutions->t2 < (float)INFINITY)
 		return (solutions);
+
 	//Intersect with side
 	tmp1 = ft_intersect_ray_with_plane(origin, direction, center, normal);
 	tmp2 = ft_intersect_ray_with_plane(origin, direction, ft_float3_plus_float3(center, ft_float_times_float3(height, normal)), normal);
@@ -36,25 +40,63 @@ t_float2 *ft_intersect_ray_with_cylinder(t_float3 origin, t_float3 direction, t_
 	solutions->t2 = tmp2.t1;
 	free(tmp1);
 	free(tmp2);
+	//On check qule point d'intersection est le plus proche de l'origine
 	if (solutions->t1 > solutions->t2)
 	{
 		t = solutions->t1;	
 		solutions->t1 = solutions->t2;
 		solutions->t2 = t;
+		center = ft_float3_plus_float3(center, ft_float_times_float3(height, normal));
 	}
 		//Check if ray pass by cylinder's normal
 	t = solutions->t1;	
 	while (t < solutions->t2)
 	{
 		t_prime = 0;
-		while (t_prime < h)
+		while (t_prime < height)
 		{
 			if (ft_float3_plus_float3(origin, ft_float_times_float3(t, direction)) == ft_float3_plus_float3(center, ft_float_times_float3(t_prime, normal)))
+			{
+				A = ft_float3_plus_float3(origin, ft_float_times_float3(t, direction));
 				break ;
+			}
 			t_prime += 1/1000;
 		}
 		t += 1/1000;
 	}
+	//IF it does
+	if (A < (float)INFINITY)
+	{
+		float B = ft_float3_plus_float3(origin, ft_float_times_float3(solutions->t1, direction)); //Determines B
+		float BC = ft_get_norm(ft_float3_minus_float3(B, center)); //Determines BC
+		float AC = ft_get_norm(ft_float3_minus_float3(A,c));
+		float AB = sqrt(AC * AC + BC * BC);
+		float APi1 = radius / BC * AB;
+		t = 0;
+		while ( t < (float)INFINITY )
+		{
+			if (ft_get_norm(ft_float3_minus_float3(A, ft_float_times_float3(direction,t))) == APi1);
+			{
+				Pi1 = ft_float3_minus_float3(A, ft_float_times_float3(direction,t));
+				break;
+			}
+			t +=  1/1000;
+		}
+		Pi2 = ft_float3_plus_float3(A, ft_float_times_float3(direction,t));
+
+		//Get ti1 and ti2 from Pi1 and Pi2
+		t = 0;
+		while (ft_float3_plus_float3(origin, ft_float_times_float3(t, direction)) != Pi1 && t < (float)INFINITY)
+			t += 1/1000;
+		solutions->t1 = t;
+		t = 0;
+		while (ft_float3_plus_float3(origin, ft_float_times_float3(t, direction)) != Pi2 && t < (float)INFINITY)
+			t += 1/1000;
+		solutions->t2 = t;
+	}
+
+
+
 	//IF it doesnt
 	else
 	{
